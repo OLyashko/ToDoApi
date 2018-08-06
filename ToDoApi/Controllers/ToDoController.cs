9181d2454bc6f6b5fa48e63531da97f50f991c66
Oleksandr Liashko <oleksandr.liashko@lebara.com>
2018-08-06 13:21:44 +0300

    Add project files.

---
+++ ToDoApi/Controllers/ToDoController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ToDoApi.Models;
+
+namespace ToDoApi.Controllers
+{
+    [Route("api/[controller]")]
+    //[ApiController]
+    //middleware pipeline ???
+    public class ToDoController : ControllerBase
+    {
+        public ToDoController(IToDoRepository p_todoItems)
+        {
+            ToDoItems = p_todoItems;
+        }
+
+        public IToDoRepository ToDoItems { get; set; }
+
+        [HttpGet]
+        public IEnumerable<ToDoItem> GetAll()
+        {
+            return ToDoItems.GetAll();
+        }
+
+        [HttpGet("{id}", Name = "GetToDo")]
+        public IActionResult GetById(string id)
+        {
+            var item = ToDoItems.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] ToDoItem item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+            ToDoItems.Add(item);
+            return CreatedAtRoute("GetToDo", new { id = item.Key }, item);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(string id, [FromBody] ToDoItem item)
+        {
+            if (item == null || item.Key != id)
+            {
+                return BadRequest();
+            }
+
+            var todo = ToDoItems.Find(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            ToDoItems.Update(item);
+            return new NoContentResult();
+        }
+
+        //Для поддержки частичных обновлений используйте HTTP PATCH ???
+        [HttpPatch("{id}")]
+        public IActionResult Update([FromBody] ToDoItem item, string id)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+
+            var todo = ToDoItems.Find(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            item.Key = todo.Key;
+
+            ToDoItems.Update(item);
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            var todo = ToDoItems.Find(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            ToDoItems.Remove(id);
+            return new NoContentResult();
+        }
+
+
+    }
+}
---
+++ ToDoApi/Models/ToDoRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoApi.Models
+{
+    public class ToDoRepository : IToDoRepository
+    {
+        private static ConcurrentDictionary<string, ToDoItem> _todos =
+            new ConcurrentDictionary<string, ToDoItem>();
+
+        public ToDoRepository()
+        {
+            Add(new ToDoItem { Name = "item1" });
+        }
+
+        public IEnumerable<ToDoItem> GetAll()
+        {
+            return _todos.Values;
+
+        }
+
+
+        public void Add(ToDoItem item)
+        {
+            item.Key = Guid.NewGuid().ToString();
+            _todos[item.Key] = item;
+
+            //throw new NotImplementedException();
+        }
+
+        public ToDoItem Find(string key)
+        {
+            ToDoItem item;
+            _todos.TryGetValue(key, out item);
+            return item;
+                        //throw new NotImplementedException();
+        }
+
+        public ToDoItem Remove(string key)
+        {
+            ToDoItem item;
+            _todos.TryRemove(key, out item);
+            return item;
+        }
+
+        public void Update(ToDoItem item)
+        {
+            _todos[item.Key] = item;
+            //throw new NotImplementedException();
+        }
+    }
+}
